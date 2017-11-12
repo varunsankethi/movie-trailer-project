57d294f1ed784a2de68c047daae0c78ef277f5dc
varunsankethi <varunsankethi@outlook.com>
2017-11-12 18:48:32 +0530

    Removed comment inside class definition

--- media.py
+++ media.py
@@ -4,9 +4,6 @@ import webbrowser
 	URLs to its poster image and youtube trailer"""
 
 class Movie():
-    """ This class provides movie details. The show_trailer() method open
-    a popup window and plays the trailer"""
-
     def __init__(self, movie_title, 
     	movie_storyline, poster_youtube, trailer_youtube):
         self.title = movie_title
