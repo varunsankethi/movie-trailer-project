56e4d6fe1fbbd0ad18b9b4b4c3ef35bf1250ae6f
varunsankethi <varunsankethi@outlook.com>
2017-11-12 18:36:58 +0530

    Implemented code review comments after the first review

--- media.py
+++ media.py
@@ -1,12 +1,14 @@
 import webbrowser
 
-"""Class describing the attributes of a movie and URLs to its poster image and youtube trailer"""
+"""Class describing the attributes of a movie and 
+	URLs to its poster image and youtube trailer"""
 
 class Movie():
     """ This class provides movie details. The show_trailer() method open
     a popup window and plays the trailer"""
 
-    def __init__(self, movie_title, movie_storyline, poster_youtube, trailer_youtube):
+    def __init__(self, movie_title, 
+    	movie_storyline, poster_youtube, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_youtube
@@ -14,5 +16,3 @@ class Movie():
 
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube)
-
-    
